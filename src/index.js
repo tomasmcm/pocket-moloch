e309e72a1858fe8d9a8a8cbc7d8a8b8c0c234b82
dekan <dekanbro@gmail.com>
2019-08-10 10:08:39 -0600

    migrate from Apollo boost

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
-import ApolloClient from 'apollo-boost';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { withClientState } from 'apollo-link-state';
+import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from 'react-apollo';
 
 import config from './config';
@@ -26,12 +32,43 @@ Amplify.configure({
   },
 });
 
-const client = new ApolloClient({
-  uri: config.GRAPH_NODE_URI,
-  clientState: {
-    resolvers,
-  },
-});
+const cache = new InMemoryCache();
+
+const client = new ApolloClient(
+  {
+    link: ApolloLink.from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors)
+          graphQLErrors.map(({ message, locations, path }) =>
+            console.log(
+              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+            ),
+          );
+        if (networkError) console.log(`[Network error]: ${networkError}`);
+      }),
+      new HttpLink({
+        uri: config.GRAPH_NODE_URI,
+        credentials: 'same-origin'
+      }),
+      withClientState({
+        defaults: {
+          isConnected: true
+        },
+        resolvers,
+        cache
+      })
+    ]),
+    cache
+  }
+);
+
+  
+// {
+//   uri: config.GRAPH_NODE_URI,
+//   clientState: {
+//     resolvers,
+//   },
+// }
 
 const Index = () => (
   <ApolloProvider client={client}>
